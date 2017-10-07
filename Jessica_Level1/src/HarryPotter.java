f44d4ec472181c07aae6cd6031c3e91dbafd6f75
Jsmith2021 <smithj2003j@gmail.com>
2017-10-07 15:58:10 -0700

    Add files via upload

---
+++ Jessica_Level1/src/HarryPotter.java
@@ -0,0 +1,48 @@
+
+public class HarryPotter {
+	
+
+		private boolean cloakOn;
+		
+		HarryPotter() {
+			System.out.println("making Harry Potter...");
+		}
+
+		void castSpell(String spell){
+	System.out.println("casting spell: " + spell);
+		}
+
+		void makeInvisible(boolean invisible) {
+			this.cloakOn = invisible;
+			
+			if (cloakOn)
+				System.out.println("Harry is invisible");
+			else
+				System.out.println("Harry is visible");
+		}
+
+		void spyOnSnape() {
+			System.out.println("Harry sees Professor Snape doing nefarious things.");
+		}
+
+		public static void main(String[] args) {
+			// 1. make harry potter
+			HarryPotter Potter=new HarryPotter();
+			// 2. become invisible
+			Potter.makeInvisible(true);
+			// 3. spy on professor snape
+			Potter.spyOnSnape();
+			// 4. become visible again
+			Potter.makeInvisible(false);
+			// 5. cast a “stupefy” spell
+			Potter.castSpell("stupefy");
+		}
+
+	}
+
+
+
+
+
+
+
