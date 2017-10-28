eed22dcaa9c5266cb0d7e87d65c4983704059ff1
Garrett-Thomas <league@WTS5.attlocal.net>
2017-10-28 15:44:08 -0700

    cuteness TV (IP)

--- src/cutenessTV.java
+++ src/cutenessTV.java
@@ -1,9 +1,28 @@
+import java.awt.event.ActionListener;
+import java.net.URI;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class cutenessTV {
-	JFrame cutenessFrame=new JFrame();
-	JPanel panel = new JPanel();
+public class cutenessTV implements ActionListener {
+	JFrame cutenessFrame;
+	JPanel panel;
+	JButton ducks;
+	JButton frogs;
+	JButton unicorns;
+	
+	public static void main(String[] args) {
+		cutenessFrame.setVisible(true);
+		cutenessFrame.add(ducks, cutenessFrame);
+		cutenessFrame.add(frogs, cutenessFrame);
+		cutenessFrame.add(unicorns, cutenessFrame);
+		cutenessFrame.setTitle("Cuteness TV!");
+		ducks.setName("Click here to watch a duck video!");
+		frogs.setName("Click here to watch a frog video!");
+		unicorns.setName("Click here to watch a unicorn video!");
+	}
+
 	
 	void showDucks() {
 		playVideo("Drirjl5K9Yk");
@@ -19,11 +38,33 @@ public class cutenessTV {
 
 	void playVideo(String videoID) {
 		try {
-	URI uri = new URI("https://www.youtube.com/v/" + videoID + "?autoplay=1");
+	URI uri = new URI("https://www.youtube.com/watch?v=" + videoID);
 			java.awt.Desktop.getDesktop().browse(uri);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public void createUI(){
+		new cutenessTV().createUI();
+		JPanel panel = new JPanel();
+		JButton ducks = new JButton();
+		JButton frogs = new JButton();
+		JButton unicorns = new JButton();
+		
+		
+		
+	}
+	public void ActionPreformed (ActionEvent e) {
+		if(e.getSource==ducks) {
+			System.out.println("button pressed");
+		}
+		if(e.getSource==frogs) {
+			System.out.println("button pressed");
+		}
+		if(e.getSource==unicorns) {
+			System.out.println("button pressed");
+		}
+	}
+	}
 
-}
