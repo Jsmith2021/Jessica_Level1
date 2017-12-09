ade4d8033684ca44a8e592cd612c015e5efc7dd1
not you <league@WTS5.attlocal.net>
2017-12-09 15:55:55 -0800

    Cow Timer, Netflix (IP)

---
+++ src/CowTimer.java
@@ -0,0 +1,46 @@
+
+import java.applet.AudioClip;
+
+import javax.swing.JApplet;
+
+public class CowTimer {
+	public static void main(String[] args) throws InterruptedException {
+		CowTimer timer = new CowTimer();
+		timer.setTime(2);
+		timer.start();
+
+	}
+
+	private int minutes;
+
+	public void setTime(int minutes) {
+		this.minutes = minutes;
+		System.out.println("Cow set to " + minutes + " minutes.");
+	}
+
+	public void start() throws InterruptedException {
+
+		for (int i = this.minutes; i > 0; i--) {
+			System.out.println(minutes);
+		}
+
+		Thread.sleep(1000);
+		playSound("moo.wav");
+
+	}
+
+	private void playSound(String fileName) {
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+		sound.play();
+
+	}
+
+	private void speak(String stuffToSay) {
+		try {
+			Runtime.getRuntime().exec("say " + stuffToSay).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
