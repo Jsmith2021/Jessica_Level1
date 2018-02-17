590a8c5c71b7c14729ca0601ba9610df0f87c145
Jsmith2021 <league@WTS-IM-6.attlocal.net>
2018-02-17 15:52:57 -0800

    Typing Tutor

--- src/TypingTutor.java
+++ src/TypingTutor.java
@@ -1,10 +1,13 @@
 import java.awt.Color;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class TypingTutor implements KeyListener{
@@ -12,6 +15,10 @@ public class TypingTutor implements KeyListener{
 	JPanel panel = new JPanel();
 	JLabel letter=new JLabel();
 	char currentLetter=generateRandomLetter();
+	static TypingTutor t1;
+	int numberOfCorrectCharacters=0;
+	int counter=0;
+	Date timeAtStart = new Date();
 	char generateRandomLetter() {
 		Random r = new Random();
 		return (char) (r.nextInt(26) + 'a');
@@ -19,7 +26,9 @@ public class TypingTutor implements KeyListener{
 
 
 	public static void main(String[] args) {
-	new TypingTutor().createUI();
+		t1=new TypingTutor();
+	t1.createUI();
+	
 
 	}	
 		
@@ -32,9 +41,10 @@ public void createUI() {
 	TypingTutor.setTitle("Type or Die");
 	letter.setText(""+currentLetter);
 	letter.setFont(letter.getFont().deriveFont(28.0f));
-	letter.setHorizontalAlignment(JLabel.CENTER);	
-	TypingTutor.add(letter);
+	letter.setHorizontalAlignment(JLabel.CENTER);
+	panel.add(letter);
 	TypingTutor.addKeyListener(this);
+	TypingTutor.pack();
 
 }
 
@@ -53,10 +63,17 @@ public void keyPressed(KeyEvent e) {
 	if(e.getKeyChar()==currentLetter) {
 		System.out.println("Correct");
 		panel.setBackground(Color.green);
+		numberOfCorrectCharacters++;
+		counter++;	
+		if(counter>10) {
+		 t1.showTypingSpeed(numberOfCorrectCharacters);
+		}
 	}
 	else {
 		panel.setBackground(Color.red);
 	}
+	
+
 }
 
 
@@ -65,5 +82,20 @@ public void keyReleased(KeyEvent e) {
 	// TODO Auto-generated method stub
 currentLetter=generateRandomLetter();
 letter.setText(""+currentLetter);
+
 }
+private void showTypingSpeed(int numberOfCorrectCharactersTyped) {
+	Date timeAtEnd = new Date();
+	long gameDuration = timeAtEnd.getTime() - timeAtStart.getTime();
+	long gameInSeconds = (gameDuration / 1000) % 60;
+	double charactersPerSecond = ((double) numberOfCorrectCharactersTyped / (double) gameInSeconds);
+	int charactersPerMinute = (int) (charactersPerSecond * 60);
+	JOptionPane.showMessageDialog(null, "Your typing speed is " + charactersPerMinute + " characters per minute.");
 }
+
+
+}
+
+
+
+
